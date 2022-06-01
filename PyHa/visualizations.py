1ac5554731c434d188c27bb91d5f4181398d8769
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2022-06-01 03:43:12 -0700

    Refactor TweetyNET automated label gen/visualizations
    - removes the tweety_output bool and moves it to isolation_parameters dict
    - updates local_score_visualization() function to reflect spectrogram_visualization() name change and organization
    - removes TweetyNET tutorial and a handful of test files
    - README documentation for TweetyNet parameters and generate_automated_labels_tweetynet()

--- PyHa/visualizations.py
+++ PyHa/visualizations.py
@@ -234,11 +234,8 @@ def local_line_graph(
 # TODO rework function so that instead of generating the automated labels, it
 # takes the automated_df as input same as it does with the manual dataframe.
 
-
 def spectrogram_visualization(
         clip_path,
-        ml_model="tweetynet",
-        tweety_output=False,
         weight_path=None,
         premade_annotations_df=None,
         premade_annotations_label="Human Labels",
@@ -331,6 +328,22 @@ def spectrogram_visualization(
                     "Skipping " +
                     clip_path +
                     " due to error in Microfaune Prediction")
+        elif (isolation_parameters["model"] == 'tweetynet'):
+            # Initializing the detector to baseline or with retrained weights
+            device = torch.device('cpu')
+            detector = TweetyNetModel(2, (1, 86, 86), 86, device, binary = False)
+
+            try:
+                #need a function to convert a signal into a spectrogram and then window it
+                tweetynet_features = compute_features([SIGNAL])
+                predictions, local_score = detector.predict(tweetynet_features, model_weights=weight_path)
+                local_scores = local_score[0].tolist()
+            except BaseException:
+                print(
+                    "Skipping " +
+                    clip_path +
+                    " due to error in TweetyNet Prediction")
+                return None
 
     # In the case where the user wants to look at automated bird labels
     if premade_annotations_df is None:
@@ -341,15 +354,28 @@ def spectrogram_visualization(
         if (isinstance(automated_df, bool) and not automated_df):
             automated_df = pd.DataFrame()
             pass
-        # For Microfaune
+        # Check if Microfaune or TweetyNET was used to generate local scores
         if (local_scores is not None):
-            automated_df = isolate(
-                    local_score[0],
-                    SIGNAL,
-                    SAMPLE_RATE,
-                    audio_dir = "",
-                    filename = "",
-                    isolation_parameters=isolation_parameters)
+            # TweetyNET techniques and output
+            if (isolation_parameters["model"] == "tweetynet"
+                and isolation_parameters["tweety_output"]):
+                automated_df = predictions_to_kaleidoscope(
+                                predictions, 
+                                SIGNAL, 
+                                "Doesn't", 
+                                "Doesn't", 
+                                "Matter", 
+                                SAMPLE_RATE)
+            # Isolation techniques
+            else: 
+                automated_df = isolate(
+                        local_score[0],
+                        SIGNAL,
+                        SAMPLE_RATE,
+                        audio_dir = "",
+                        filename = "",
+                        isolation_parameters=isolation_parameters)
+        # Catch, generate the labels for other models
         else:
             automated_df = generate_automated_labels(
                 audio_dir=clip_path,
@@ -386,108 +412,6 @@ def spectrogram_visualization(
             premade_annotations_df=premade_annotations_df,
             premade_annotations_label=premade_annotations_label,
             save_fig=save_fig)
-
-def local_score_visualization_tweetynet(
-        clip_path,
-        tweety_output=False,
-        weight_path=None,
-        premade_annotations_df=None,
-        premade_annotations_label="Human Labels",
-        automated_df=False,
-        isolation_parameters=None,
-        log_scale=False,
-        save_fig=False,
-        normalize_local_scores=False):
-    """
-    Wrapper function for the local_line_graph function for ease of use.
-    Processes clip for local scores to be used for the local_line_graph
-    function.
-
-    Args:
-        clip_path (string)
-            - Path to an audio clip.
-
-        tweety_output (boolean) # may want to incorporate into isolation parameters
-            - True to use tweetynet's original output, or False to use 
-
-        weight_path (string)
-            - Weights to be used for RNNDetector.
-
-        premade_annotations_df (Dataframe)
-            - Dataframe of annotations to be displayed that have been created
-              outside of the function.
-        premade_annotations_label (string)
-            - String that serves as the descriptor for the premade_annotations
-              dataframe.
-
-        automated_df (Dataframe)
-            - Whether the audio clip should be labelled by the isolate function
-              and subsequently plotted.
-
-        log_scale (boolean)
-            - Whether the axis for local scores should be logarithmically
-              scaled on the plot.
-
-        save_fig (boolean)
-            - Whether the plots should be saved in a directory as a png file.
-
-    Returns:
-        None
-    """
-
-    # Loading in the clip with Microfaune's built-in loading function
-    try:
-        SAMPLE_RATE, SIGNAL = audio.load_wav(clip_path)
-    except BaseException:
-        print("Failure in loading", clip_path)
-        return
-    # downsample the audio if the sample rate > 44.1 kHz
-    # Force everything into the human hearing range.
-    try:
-        if SAMPLE_RATE > 44100:
-            rate_ratio = 44100 / SAMPLE_RATE
-            SIGNAL = scipy_signal.resample(
-                SIGNAL, int(len(SIGNAL) * rate_ratio))
-            SAMPLE_RATE = 44100
-    except BaseException:
-        print("Failure in downsampling", clip_path)
-        return
-
-    # Converting to Mono if Necessary
-    if len(SIGNAL.shape) == 2:
-        # averaging the two channels together
-        SIGNAL = SIGNAL.sum(axis=1) / 2
-
-    # Initializing the detector to baseline or with retrained weights
-    device = torch.device('cpu')
-    detector = TweetyNetModel(2, (1, 86, 86), 86, device, binary = False)
-
-    try:
-        #need a function to convert a signal into a spectrogram and then window it
-        tweetynet_features = compute_features([SIGNAL])
-        predictions, local_score = detector.predict(tweetynet_features, model_weights=weight_path)
-    except BaseException:
-        print(
-            "Skipping " +
-            clip_path +
-            " due to error in TweetyNet Prediction")
-        return None
-
-    # In the case where the user wants to look at automated bird labels
-    if premade_annotations_df is None:
-        premade_annotations_df = pd.DataFrame()
-    if automated_df:
-        if tweety_output:
-            automated_df = predictions_to_kaleidoscope(predictions, SIGNAL, "Doesn't", "Doesn't", "Matter", SAMPLE_RATE)
-        else:
-            automated_df = isolate(
-                local_score[0],
-                SIGNAL,
-                SAMPLE_RATE,
-                "Doesn't",
-                "Matter",
-                isolation_parameters,
-                normalize_local_scores=normalize_local_scores)
 
 def binary_visualization(automated_df, human_df, save_fig=False):
     """
