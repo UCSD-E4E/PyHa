07c213576d2999c7d0b4c0d817f4af2ae1d37b30
Vanessa-Salgado <vsalgadozavaleta@ucsb.edu>
2022-07-07 18:08:52 -0700

    script to produce ROC curves

--- PyHa/annotation_post_processing.py
+++ PyHa/annotation_post_processing.py
@@ -174,8 +174,7 @@ def generate_ROC_curves(automated_df, manual_df, local_scoress, chunk_length = 3
     plt.ylable("True Postives")
     plt.xlable("False Positives ")
     #display.plot()
-    plt.show    
-
+    plt.show
 
 
 
