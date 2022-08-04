4ce366f282730dcc17f62a918fde7a95eef7e8ad
Sean Perry <sean.hyatt.perry@gmail.com>
2022-08-03 17:01:47 -0700

    Numpy error with dep investigate

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup, find_packages
 from pydoc import ModuleScanner 
 
 
-modules = []
-def callback(path, modname, desc, modules=modules):
-    if modname and modname[-9:] == '.__init__':
-        modname = modname[:-9] + ' (package)'
-    if modname.find('.') < 0:
-        modules.append(modname)
-def onerror(modname):
-    callback(None, modname, None)
+modules = ["Keras", "librosa", "matplotlib", "microfaune-ai", "numpy",
+    "opensoundscape", "pandas", "pydub", "scikit-image", "scikit-learn",
+    "scipy", "seaborn", "SoundFile", "tensorflow", "torch", "freetype-py"
+]
+#blacklist_mod = ["multiprocessing", 'raystreaming', 'formatter' ]
+#def callback(path, modname, desc, modules=modules):
+#    if modname and modname[-9:] == '.__init__':
+#        modname = modname[:-9] + ' (package)'
+#    if modname.find('.') < 0 and modname[0] != '_'and modname[0] != '~' and not(modname in blacklist_mod):
+#        modules.append(modname)
+#def onerror(modname):
+#    callback(None, modname, None)#
 
-ModuleScanner().run(callback, onerror=onerror)
+#ModuleScanner().run(callback, onerror=onerror)
 
 print(modules)
 
@@ -19,7 +23,7 @@ print(modules)
 setup(
        # the name must match the folder name 'verysimplemodule'
         name="PyHa_test", 
-        version="0.0.14",
+        version="0.0.19",
         author="Jacob Ayers, Sean Perry, Sam Prestrelski, Vannessa Salgado",
         #author_email="<youremail@email.com>",
         description="A python package for automatically detecting species and comparing to ground truth",
@@ -31,7 +35,7 @@ setup(
         keywords=['python'],
         classifiers= [
             "Development Status :: 3 - Alpha",
-            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
         ]
