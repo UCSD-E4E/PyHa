8ce0a9e7d2046e4046908c24cccf924facda96fb
natmfat <natmfat@gmail.com>
2024-01-23 17:13:15 -0800

    [📝] Add Stuff from README

---
+++ PyHa/__init__.py
@@ -0,0 +1,7 @@
+r"""
+A tool designed to convert audio-based "weak" labels to "strong" moment-to-moment labels. Provides a pipeline to compare automated moment-to-moment labels to human labels. Current proof of concept work being fulfilled on Bird Audio clips using Microfaune predictions.
+
+This package is being developed and maintained by the [Engineers for Exploration Acoustic Species Identification Team](http://e4e.ucsd.edu/acoustic-species-identification) in collaboration with the [San Diego Zoo Wildlife Alliance](https://sandiegozoowildlifealliance.org/).
+
+PyHa = Python + Piha (referring to a bird species of our interest known as the screaming-piha)
+"""
